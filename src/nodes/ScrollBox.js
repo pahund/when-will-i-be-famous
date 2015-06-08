4a6b2e78bc7771599efee6ada56e71c067fd664d
Patrick Hund <pahund@ebay.com>
2015-06-08 16:04:14 +0200

    refactoring
    Change-Id: I04cb406ea80c3733c3a59ef4f446137032795df3

--- src/nodes/ScrollBox.js
+++ src/nodes/ScrollBox.js
@@ -5,39 +5,46 @@ import Mover from "../components/Mover";
 import calculator from "../calculator";
 import viewportSizeF from "../getViewportSize";
 
+const handleDrag = Symbol("handle drag");
+
 class ScrollBox {
     constructor(parent) {
-        const viewportSize = viewportSizeF();
         let gestures;
 
+        this.viewportSize = viewportSizeF();
         this.node = parent.addChild();
+
         gestures = new GestureHandler(this.node);
 
         createDOMElement(this.node);
 
-        gestures.on("drag", (data) => {
-            const currentY = getCoords(this.node).y,
-                gallerySize = calculator.getGallerySize(),
-                maxY = 0,
-                minY = (gallerySize.h * -1) + viewportSize.h;
-            let newY = currentY + (data.status === "end" ? data.centerVelocity.y / 2 : data.centerDelta.y);
-            if (newY > maxY) {
-                newY = maxY;
-            }
-            if (newY < minY) {
-                newY = minY;
-            }
-            if (data.status === "end") {
-                const mover = new Mover(this.node, { x: null, y: newY }, 500, "easeOut");
-                mover.start();
-                return;
-            }
-            this.node.setPosition(null, newY);
-        });
+        gestures.on("drag", this[handleDrag].bind(this));
     }
 
     static add(parent) {
         return new ScrollBox(parent).node;
+    }
+
+    ////////// PRIVATE METHODS //////////
+
+    [handleDrag]({ status, centerVelocity, centerDelta }) {
+        const currentY = getCoords(this.node).y,
+            gallerySize = calculator.getGallerySize(),
+            maxY = 0,
+            minY = (gallerySize.h * -1) + this.viewportSize.h;
+        let newY = currentY + (status === "end" ? centerVelocity.y / 2 : centerDelta.y);
+        if (newY > maxY) {
+            newY = maxY;
+        }
+        if (newY < minY) {
+            newY = minY;
+        }
+        if (status === "end") {
+            const mover = new Mover(this.node, { x: null, y: newY }, 500, "easeOut");
+            mover.start();
+            return;
+        }
+        this.node.setPosition(null, newY);
     }
 }
 
