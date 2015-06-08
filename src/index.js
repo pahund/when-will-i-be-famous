e35cb59cbbffb5b3846ebc107ee8df87f759b7ec
Patrick Hund <pahund@ebay.com>
2015-06-08 12:49:44 +0200

    refactoring
    Change-Id: Ie566f128fc9793d9023d749169ec3f044bd894ce

--- src/index.js
+++ src/index.js
@@ -1,40 +1,11 @@
-import GestureHandler from "famous/components/GestureHandler";
-import createDOMElement from "./createDOMElement";
 import addCar from "./addCar";
+import addContainer from "./addContainer";
 import delay from "./delay";
 import scene from "./scene";
-import getCoords from "./getCoords";
-import Mover from "./components/Mover";
 import settings from "./settings";
-import calculator from "./calculator";
-import viewportSizeF from "./getViewportSize";
 
-const container = scene.addChild(),
-    gestures = new GestureHandler(container),
-    viewportSize = viewportSizeF();
-
-createDOMElement(container);
+const container = addContainer(scene);
 
 for (let i = 0; i < settings.numberOfThumbnails; i++) {
     delay((i * 30) + (Math.random() * 100), addCar)(container, i);
 }
-gestures.on("drag", function (data) {
-    const currentY = getCoords(container).y,
-        gallerySize = calculator.getGallerySize(),
-        maxY = 0,
-        minY = (gallerySize.h * -1) + viewportSize.h;
-    let newY = currentY + (data.status === "end" ? data.centerVelocity.y / 2 : data.centerDelta.y);
-    if (newY > maxY) {
-        newY = maxY;
-    }
-    if (newY < minY) {
-        newY = minY;
-    }
-    if (data.status === "end") {
-        const mover = new Mover(container, { x: null, y: newY }, 500, "easeOut");
-        mover.start();
-        return;
-    }
-    container.setPosition(null, newY);
-});
-
